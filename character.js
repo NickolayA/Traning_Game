747099878e578517bf2259978d648cc2a6ed29cd
vipz3 <vipz3@mail.ru>
2018-09-25 13:25:59 +0300

    Character class is created

---
+++ character.js
@@ -0,0 +1,7 @@
+var Character = (function (startPositionX, startPositionY, countOfBoxes) {
+    var characterSingleton;
+
+    function constructor(startPositionX, startPositionY, countOfBoxes) {
+
+    }
+})(startPositionX, startPositionY);
